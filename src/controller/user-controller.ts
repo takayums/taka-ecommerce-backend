a9948d9888daea8702f0e67753db2f47b8ee2272
takayums <takayums20@gmail.com>
2024-12-16 22:50:07 +0700

    feat(users): change modular file feat login and registeri & add response error custom and middleware

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,97 +1,31 @@
-import { Response, Request } from "express";
-import "dotenv/config";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { prismaClient } from "../application/database";
-import { responseUser } from "../model/user-model";
+import { Response, Request, NextFunction } from "express";
+import { CreateUserRequest, LoginRequest } from "../model/user-model";
+import { UserService } from "../service/user-service";
 
 export class User {
-  static async Register(req: Request, res: Response) {
+  static async Register(req: Request, res: Response, next: NextFunction) {
     try {
-      const registerRequest = req.body;
-      const existingUser = await prismaClient.user.count({
-        where: {
-          email: registerRequest.email,
-        },
-      });
-
-      if (existingUser == 1) {
-        res.status(400).json({ message: "User is already" });
-      }
-
-      registerRequest.password = await bcrypt.hash(
-        registerRequest.password,
-        10,
-      );
-
-      const user = await prismaClient.user.create({
-        data: registerRequest,
-      });
-
-      const data = responseUser(user);
-
-      res.status(201).json({
-        message: "register succesfully",
-        success: true,
-        data: data,
+      const request: CreateUserRequest = req.body as CreateUserRequest;
+      const response = await UserService.RegisterService(request);
+      res.status(200).json({
+        data: response,
+        message: "User Created",
+        status: true,
       });
     } catch (error) {
-      res.json({
-        message: `register failed, ${error}`,
-        status: "error",
-      });
+      next(error);
     }
   }
 
-  static async Login(req: Request, res: Response) {
+  static async Login(req: Request, res: Response, next: NextFunction) {
     try {
-      const loginRequest = req.body;
-      const jwtSecretKey: string = process.env.SECRETE_KEY as string;
-      let user = await prismaClient.user.findUnique({
-        where: {
-          email: loginRequest.email,
-        },
-      });
-
-      if (!user) {
-        res.status(404).json({ message: "User not found", success: false });
-      }
-
-      const passwordMatched = await bcrypt.compare(
-        loginRequest.password,
-        user!.password,
-      );
-
-      if (!passwordMatched) {
-        res
-          .status(400)
-          .json({ message: "Email or Password is wrong", success: false });
-      }
-
-      const token = jwt.sign({ data: user?.email }, jwtSecretKey, {
-        expiresIn: "1h",
-      });
-
-      user = await prismaClient.user.update({
-        where: { email: loginRequest.email },
-        data: {
-          token: token,
-        },
-      });
-
-      const data = responseUser(user);
-      data.token = user.token!;
-
+      const request: LoginRequest = req.body as LoginRequest;
+      const response = await UserService.LoginService(request);
       res.status(200).json({
-        message: "Login Success",
-        success: true,
-        data: data,
+        data: response,
       });
     } catch (error) {
-      res.status(400).json({
-        message: `Login failed, ${error}`,
-        success: false,
-      });
+      next(error);
     }
   }
 
---
+++ src/service/user-service.ts
@@ -0,0 +1,73 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { prismaClient } from "../application/database";
+import { logger } from "../application/logging";
+import { ResponseError } from "../error/response-error";
+import {
+  CreateUserRequest,
+  LoginRequest,
+  responseUser,
+} from "../model/user-model";
+
+export class UserService {
+  static async RegisterService(request: CreateUserRequest) {
+    const existingUser = await prismaClient.user.count({
+      where: {
+        email: request.email,
+      },
+    });
+
+    if (existingUser == 1) {
+      logger.warn(
+        `Registration failed: Email already is use - ${request.email}`,
+      );
+      throw new ResponseError("User is already", 401);
+    }
+
+    request.password = await bcrypt.hash(request.password, 10);
+
+    const user = await prismaClient.user.create({
+      data: request,
+    });
+
+    const data = responseUser(user);
+    return data;
+  }
+
+  static async LoginService(request: LoginRequest) {
+    const jwtSecretKey: string = process.env.SECRETE_KEY as string;
+    let user = await prismaClient.user.findUnique({
+      where: {
+        email: request.email,
+      },
+    });
+
+    if (!user) {
+      throw new ResponseError("User not found", 404);
+    }
+
+    const passwordMatched = await bcrypt.compare(
+      request.password,
+      user!.password,
+    );
+
+    if (!passwordMatched) {
+      throw new ResponseError("Email or Password is wrong", 400);
+    }
+
+    const token = jwt.sign({ data: user?.email }, jwtSecretKey, {
+      expiresIn: "1h",
+    });
+
+    user = await prismaClient.user.update({
+      where: { email: request.email },
+      data: {
+        token: token,
+      },
+    });
+
+    const data = responseUser(user);
+    data.token = user.token!;
+    return data;
+  }
+}
